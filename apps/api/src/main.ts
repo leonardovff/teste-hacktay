8f9ef8a071a7c97acbcca56866851aeed6e2a2a5
lvff <leonardovff@gmail.com>
2021-09-19 13:55:15 -0300

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api';
+  app.enableCors();
   app.setGlobalPrefix(globalPrefix);
 
   const port = process.env.PORT || 3333;
